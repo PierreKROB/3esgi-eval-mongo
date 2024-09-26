a82120fdfd0a6083c9d7c7d72b55cfcbd6c027d3
McCat1701 <lafere.mic@hotmail.fr>
2024-09-26 10:57:31 +0200

    controleur commentaire

--- controller/comment.controller.js
+++ controller/comment.controller.js
@@ -7,13 +7,32 @@ const Comment = require("./../model/comment.model");
  *     message: <string>,
  *     userId: <string>,
  *     postId: <string>
- * }
+ * }r
  */
-exports.create = async () => {
-    try{
+exports.create = async (req, res, next) => {
+    try {
         //TODO
+        if (req.body.message == undefined || req.body.userId == undefined || req.body.postId == undefined) {
+            return res.status(400).json({ message: 'Merci de renseigner les paramètres nécessaires.' });
+        }
+        else {
+            message = req.body.message
+            userId = req.body.userId
+            postId = req.body.userId
+
+        }
+
+        const comment = new Comment({
+            message,
+            userId,
+            postId
+        })
+        await Comment.save();
+
         res.status(201).json(comment);
-    }catch(e){
+    }
+
+    catch (e) {
         res.status(500).json(e.message);
     }
 }
@@ -26,11 +45,23 @@ exports.create = async () => {
  *     message: <string>,
  * }
  */
-exports.update = async () => {
-    try{
+exports.update = async (req, res, next) => {
+    try {
+
+
+        const updatedata = req.body.message;
         //TODO
-        res.status(200).json({message: "Commentaire mis à jour"});
-    }catch(e){
+        const comments = await Comment.findOnAndUpdate(
+            { id: req.param.id },
+            { $set: { message: updatedata } },
+            { new: true, runValidators: true }
+
+        )
+        if (!comments) {
+            return res.status(404).json({ message: 'comment not find.' });
+        }
+        res.status(200).json({ message: "Commentaire mis à jour" });
+    } catch (e) {
         res.status(500).json(e.message);
     }
 }
@@ -39,11 +70,17 @@ exports.update = async () => {
  * Méthode pour supprimer un commentaire
  * @param id l'id du commentaire à supprimer
  */
-exports.delete = async () => {
-    try{
+exports.delete = async (res, req, next) => {
+    try {
         //TODO
-        res.status(200).json({message: "Commentaire supprimé"});
-    }catch(e){
+        const comment = await Comment.findByIdAndDelete(req, param.id)
+
+        if (!comment) {
+            return res.status(404).json({ message: 'commentaire non trouvé' });
+        }
+
+        res.status(200).json({ message: "Commentaire supprimé" });
+    } catch (e) {
         res.status(500).json(e.message);
     }
 }
