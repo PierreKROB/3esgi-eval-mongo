1dca79dab9f5b7fe0f6729befb01b12c7c7f3927
PierreKROB <krobpierre0@gmail.com>
2024-09-26 09:25:11 +0200

    add post model

--- model/post.model.js
+++ model/post.model.js
@@ -3,3 +3,13 @@
  * 
  * Un post doit avoir au minimum : un message, une date, un userId (auteur du post)
  */
+
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    message: { type: String, required: true },
+    date: { type: Date, default: Date.now, required: true },
+    userId: { type: String, required: true }
+});
+
+const Post = mongoose.model('Post', postSchema);
