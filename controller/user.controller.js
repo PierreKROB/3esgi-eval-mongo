57a253b109fa0d5407fb32d4c87d9728043ce2a6
PierreKROB <krobpierre0@gmail.com>
2024-09-26 11:36:42 +0200

    fix usercontroller

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -9,18 +9,18 @@ const bcrypt = require("bcrypt");
  *     password: <string>
  * }
  */
-exports.login = async (req,res) => {
-    try{
-            //TODO
-            const user = await User.findOne({email: req.body.email});
-            
-            if(bcrypt.compare(req.body.password, user.password)){
-                res.status(200).send("connexion reussi.");
-            }
-            else
-                res.status(401).send("le mot de passe n'est pas correct.")
+exports.login = async (req, res) => {
+    try {
+        //TODO
+        const user = await User.findOne({ email: req.body.email });
+
+        if (bcrypt.compare(req.body.password, user.password)) {
+            res.status(200).send("connexion reussi.");
+        }
+        else
+            res.status(401).send("le mot de passe n'est pas correct.")
     }
-    catch(e){
+    catch (e) {
         res.status(500).json(e.message);
     }
 }
@@ -34,19 +34,19 @@ exports.login = async (req,res) => {
  *     username: <string>
  * }
  */
-exports.signin= async (req,res) => {
-    try{
-        //TODO
-            let newUser = {
-                 ...req.body,
-                password : bcrypt.hashSync(req.body.password,10)
-            }
-            await User.create(newUser);
-        
+exports.signin = async (req, res) => {
+    try {
+        const password = req.body.password;
 
-        res.status(200).json(listUser);
+        const newUser = {
+            ...req.body,
+            password: bcrypt.hashSync(password, 10)
+        }
+        await User.create(newUser);
+
+        res.status(200).json(newUser);
     }
-    catch(e){
+    catch (e) {
         res.status(500).json(e.message);
     }
 }
