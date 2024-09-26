fefd0fb9b17f6c3b0c18e7955e096e951dd3e321
McCat1701 <lafere.mic@hotmail.fr>
2024-09-26 10:10:14 +0200

    test

--- model/comment.model.js
+++ model/comment.model.js
@@ -3,3 +3,14 @@
  * 
  * Un post doit avoir au minimum : un message, une date, un userId (auteur du post) et un postId
  */
+
+const mongoose = require('mongoose');
+
+const commentSchema = mongoose.Schema({
+    message: { type: String, required: true, unique: true },
+    date:{type: Date, default: Date.now ,required:true },
+    userId:{type :mongoose.Schema.Types.ObjectId ,ref:'User' ,required : true},
+    postId:{type :mongoose.Schema.Types.ObjectId ,ref:'Post' ,required : true},
+    }
+); 
+module.exports = mongoose.model('Comment', commentSchema);
