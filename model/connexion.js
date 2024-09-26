0b88299140f324ae6ff9006fc9564ff5947ae72e
PierreKROB <krobpierre0@gmail.com>
2024-09-26 11:12:52 +0200

    add connection fix

--- model/connexion.js
+++ model/connexion.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect(){
     try{
-        await mongoose.connect("mongodb://127.0.0.1:27017/esgi");
+        await mongoose.connect("mongodb://127.0.0.1:27017/evaluation");
         console.log("connexion mongo réussie!");
     }catch(e){
         console.error("connexion echouée : " + e.message);
